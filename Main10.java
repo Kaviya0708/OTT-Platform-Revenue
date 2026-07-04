9ee4051ad4ee1fb5789a86b68c1b6d9c643c2d73
Kaviya <732125104051.25.cse@nandhatech.org>
2026-07-04 08:21:11 +0530

    Create Main10.java

---
+++ Main10.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        int fee = 299;
+        int users = 600;
+        int revenue = fee * users;
+
+        System.out.println("Subscription Fee = " + fee);
+        System.out.println("Users = " + users);
+        System.out.println("Revenue = " + revenue);
+    }
+}
